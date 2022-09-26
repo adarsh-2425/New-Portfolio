1378aee150ff2a900d3c231cc5ebb51c553bc804
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2022-09-26 12:21:34 +0530

    navbar added

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
-{path : '' , component : WelcomeComponent}
+{path : '' , component : WelcomeComponent},
+{path : 'navbar' , component : NavbarComponent}
 ];
 
 @NgModule({
