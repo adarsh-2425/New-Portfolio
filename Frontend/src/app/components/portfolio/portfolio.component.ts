4ea1fbe106398fbaa12c4288cbe0dbd5ae0d2f57
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-10-23 08:27:05 +0530

    123

--- Frontend/src/app/components/portfolio/portfolio.component.ts
+++ Frontend/src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInOut } from 'src/app/animations/animations';
+import { fade } from 'src/app/animations/fade';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
+  animations: [
+    slideInOut,
+    fade
+  ]
 })
 export class PortfolioComponent implements OnInit {
+  visible: any;
 
   constructor() { }
 
