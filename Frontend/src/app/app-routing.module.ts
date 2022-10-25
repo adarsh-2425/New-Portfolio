46028382bcdd30025122c2e6c4ac02f60f4d7edd
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-10-25 15:56:33 +0530

    navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ContactComponent } from './components/contact/contact.component'
+
 
 const routes: Routes = [
-{path : '' , component : WelcomeComponent},
+{path : '' , component : AboutComponent},
 {path : 'navbar' , component : NavbarComponent},
 {path : 'about' , component : AboutComponent},
 {path : 'portfolio' , component : PortfolioComponent}
