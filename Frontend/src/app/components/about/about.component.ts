122d9616b0648a95614dcfeecc3ce50115405dba
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-10-28 14:42:31 +0530

    lol

--- Frontend/src/app/components/about/about.component.ts
+++ Frontend/src/app/components/about/about.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import skills from '../../../assets/json/skills.json';
 import { slideInOut } from 'src/app/animations/animations';
 import { fade } from 'src/app/animations/fade';
 
+interface Skills{
+  id: Number,
+  name: string,
+  percentage: number
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -13,7 +19,7 @@ import { fade } from 'src/app/animations/fade';
 })
 export class AboutComponent implements OnInit {
 visible: any;
-
+skills: Skills[] = skills; 
   constructor() { }
 
   ngOnInit(): void {
