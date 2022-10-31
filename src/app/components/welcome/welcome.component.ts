125c798847f828b6fb9cbf9a3d0ad826ad954ce3
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-10-31 10:25:26 +0530

    update

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { slideInOut } from 'src/app/animations/animations';
 import { fade } from 'src/app/animations/fade';
 
@@ -13,10 +14,17 @@ import { fade } from 'src/app/animations/fade';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     
   }
 
+  about(){
+    console.log('lol');
+    
+    // this.router.navigate(['/about'])
+  }
 }
